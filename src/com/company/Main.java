dcf3aa8e90d9109873493fe2aa71eaa37fd4f617
andrew <andrewoliveira282@gmail>
2021-10-28 13:31:50 -0300

    exercicio de set

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//ler um vetor e organiza ele
+        List<Integer> l=new ArrayList<>();
+        l.add(10);
+        l.add(20);
+        System.out.println(l);
+    }
+}
